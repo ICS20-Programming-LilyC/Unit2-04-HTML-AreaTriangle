0db7f3d4fca9148e91ecf38e0df082375249800e
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-03-21 14:02:40 +0000

    Attempted to fix linter errors

--- js/script.js
+++ js/script.js
@@ -10,11 +10,11 @@
  */
 function calculate () {
   // input
-  let length = parseInt(document.getElementById('length-of-rectangle').value);
-  let width = parseInt(document.getElementById('width-of-rectangle').value);
+  let base = parseInt(document.getElementById('base-of-rectangle').value);
+  let height = parseInt(document.getElementById('height-of-rectangle').value);
 
   // process
-  let area = length * width / 2;
+  let area = base * height / 2;
 
   // output
   document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
